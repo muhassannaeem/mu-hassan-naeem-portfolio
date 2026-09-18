c849c163f7a1d31e2040767b8b25c50cb9d520ce
muhassannaeem <muhassannaeem@gmail.com>
2026-09-19 02:46:25 +0500

    Remove technical arsenal section

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ import Navbar from '@/components/layout/Navbar';
 import Footer from '@/components/layout/Footer';
 import Hero from '@/components/sections/Hero';
 import About from '@/components/sections/About';
-import TechArsenal from '@/components/sections/TechArsenal';
 import FeaturedProjects from '@/components/sections/FeaturedProjects';
 import ServicesOffered from '@/components/sections/Services';
 import Contact from '@/components/sections/Contact';
@@ -13,7 +12,6 @@ export default function Home() {
       <Navbar />
       <Hero />
       <About />
-      <TechArsenal />
       <FeaturedProjects />
       <ServicesOffered />
       <Contact />
